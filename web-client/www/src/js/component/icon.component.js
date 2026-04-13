b6d4d89c2b349cc4548a056b4b7b6028f8498faa
charles ligony <c-ligony@efluid.fr>
2026-04-13 13:54:51 +0200

    feat(auth): add recovery workflow

--- web-client/www/src/js/component/icon.component.js
+++ web-client/www/src/js/component/icon.component.js
@@ -470,6 +470,24 @@ class PictureCameraIcon extends HTMLElement {
     }
 }
 
+class LockIcon extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+            <path fill-rule="evenodd" d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3c0-2.9-2.35-5.25-5.25-5.25Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z" clip-rule="evenodd" />
+        </svg>`
+    }
+}
+
+class KeyIcon extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+            <path fill-rule="evenodd" d="M15.75 1.5a6.75 6.75 0 0 0-6.651 7.906c.067.39-.032.717-.221.906l-6.5 6.499a3 3 0 0 0-.878 2.121v2.818c0 .414.336.75.75.75H6a.75.75 0 0 0 .75-.75v-1.5h1.5A.75.75 0 0 0 9 19.5V18h1.5a.75.75 0 0 0 .53-.22l2.658-2.658c.19-.189.517-.288.906-.22A6.75 6.75 0 1 0 15.75 1.5Zm0 3a.75.75 0 0 0 0 1.5A2.25 2.25 0 0 1 18 8.25a.75.75 0 0 0 1.5 0 3.75 3.75 0 0 0-3.75-3.75Z" clip-rule="evenodd" />
+        </svg>`
+    }
+}
+
 export const icons = {
     'revoice-icon-pencil': PencilIconComponent,
     'revoice-icon-trash': TrashIconComponent,
@@ -514,6 +532,8 @@ export const icons = {
     'revoice-icon-download': DownloadIcon,
     'revoice-icon-open-in-browser': OpenInBrowserIcon,
     'revoice-icon-picture-camera': PictureCameraIcon,
+    'revoice-icon-lock': LockIcon,
+    'revoice-icon-key': KeyIcon,
 }
 
 for (let iconsKey in icons) {
